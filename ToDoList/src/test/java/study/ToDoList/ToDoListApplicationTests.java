10c81bd4eab975548b75a46eeacb12cbd2ae458d
book0256@naver.com <book0256@naver.com>
2024-03-13 21:17:03 +0900

    initial commit

---
+++ ToDoList/src/test/java/study/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
