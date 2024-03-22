12ee4d26cc2515c0ac2f90bae0ed3d0c83ad20e9
이건희 <022686s@gmail.com>
2024-03-22 09:24:06 +0900

    파일 경로 수정

--- front/src/js/sign-up.js
+++ front/src/js/sign-up.js
@@ -1,34 +1,28 @@
 function init() {
-    var signUpBtn = document.getElementById("signUpBtn");
+  var signUpBtn = document.getElementById("signUpBtn");
 
-    signUpBtn.onclick = signUp;
-
-    var signInLink = document.getElementsByClassName("sign-in-link")[0];
-    signInLink.onclick = () => {
-        console.log("로그인 페이지로 이동");
-    }
+  signUpBtn.onclick = signUp;
 }
 
 function signUp(e) {
-    var signUpDataElements = e.target.parentNode.children;
-    var signUpData = {};
-    
-    signUpData["name"] = signUpDataElements[0].value;
-    
-    var genderRadioElements = signUpDataElements[1].children;
-    if(genderRadioElements[1].checked === true) {
-        signUpData["gender"] = genderRadioElements[1].value;
-    }
-    else {
-        signUpData["gender"] = genderRadioElements[3].value;
-    }
+  var signUpDataElements = e.target.parentNode.children;
+  var signUpData = {};
 
-    signUpData["birthday"] = signUpDataElements[2].children[1].value;
-    signUpData["hometown"] = signUpDataElements[3].children[1].value;
-    signUpData["id"] = signUpDataElements[4].value;
-    signUpData["password"] = signUpDataElements[5].value;
+  signUpData["name"] = signUpDataElements[0].value;
 
-    console.log(signUpData);    
+  var genderRadioElements = signUpDataElements[1].children;
+  if (genderRadioElements[1].checked === true) {
+    signUpData["gender"] = genderRadioElements[1].value;
+  } else {
+    signUpData["gender"] = genderRadioElements[3].value;
+  }
+
+  signUpData["birthday"] = signUpDataElements[2].children[1].value;
+  signUpData["hometown"] = signUpDataElements[3].children[1].value;
+  signUpData["id"] = signUpDataElements[4].value;
+  signUpData["password"] = signUpDataElements[5].value;
+
+  console.log(signUpData);
 }
 
 init();
